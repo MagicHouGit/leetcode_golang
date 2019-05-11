1f94f4933a2a13bb050ae8c7374a046da20bcdbd
MagicHouGit <hyzmonkey@163.com>
2019-05-11 09:11:21 +0800

    rename

--- 217containsDuplicate.go
+++ 217containsDuplicate.go
@@ -1,18 +1,19 @@
+//存在重复元素
 package main
 
 import "fmt"
 
-func main (){
-	test_nums := []int{1,2,3,1}
-    fmt.Println(containsDuplicate(test_nums))
+func main() {
+	test_nums := []int{1, 2, 3, 1}
+	fmt.Println(containsDuplicate(test_nums))
 }
 func containsDuplicate(nums []int) bool {
-	if len(nums) == 1{
+	if len(nums) == 1 {
 		return false
 	}
-	for i:= 0;i < len(nums)-1;i++{
-		for j:= i+1; j<len(nums);j++ {
-			if nums[i]== nums[j]{
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] == nums[j] {
 				return true
 			}
 
