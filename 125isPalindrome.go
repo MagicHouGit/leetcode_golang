49025dcecfe5047d1d45da5d1a2dd7410edc2247
MagicHouGit <hyzmonkey@163.com>
2019-06-11 11:08:04 +0800

    first commit

---
+++ 125isPalindrome.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	// TmpS := "A man, a plan, a canal: Panama"
+	TmpS := "0P"
+	fmt.Printf("%t\n", isPalindrome(TmpS))
+}
+
+/*
+执行用时 : 4 ms, 在Valid Palindrome的Go提交中击败了97.76% 的用户
+内存消耗 : 4.6 MB, 在Valid Palindrome的Go提交中击败了24.33% 的用户
+//*/
+func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	tmp := []rune{}
+	for _, v := range s {
+		if v >= 97 && v <= 122 {
+			tmp = append(tmp, v)
+		}
+		if v >= 48 && v <= 57 {
+			tmp = append(tmp, v)
+		}
+	}
+	l := len(tmp)
+	//进行首尾比较
+	for i := 0; i < (l+1)/2; i++ {
+		if tmp[i] != tmp[l-i-1] {
+			return false
+		}
+	}
+	// for i, v := range tmp {
+	// 	fmt.Printf("%d-%c\n", i, v)
+	// }
+	return true
+
+}
+
+/*
+借鉴一下leetcode上的其他解法
+//*/
+func isPalindrome(s string) bool {
+	start, end := 0, len(s)-1
+	for start < end {
+		sc, ec := s[start], s[end]
+		if sc >= 'A' && sc <= 'Z' {
+			sc += 32
+		}
+		if ec >= 'A' && ec <= 'Z' {
+			ec += 32
+		}
+
+		if !((sc >= '0' && sc <= '9') || (sc >= 'a' && sc <= 'z')) {
+			start++
+			continue
+		}
+		if !((ec >= '0' && ec <= '9') || (ec >= 'a' && sc <= 'z')) {
+			end--
+			continue
+		}
+		if sc != ec {
+			return false
+		}
+		start++
+		end--
+	}
+	return true
+}
