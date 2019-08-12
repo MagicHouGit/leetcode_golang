2f037d43c528069d0597b30b3ff79157ff065c98
OMPower <magicmac@houshisandeMacBook-Pro.local>
2019-08-12 19:46:58 +0800

    first commit

---
+++ 234isPalindrome.go
@@ -0,0 +1,99 @@
+//执行用时 :28 ms, 在所有 Go 提交中击败了46.04%的用户
+//内存消耗 :6.5 MB, 在所有 Go 提交中击败了33.61%的用户
+package main
+
+import "fmt"
+
+func main() {
+	//创造一个回文联表
+	tmpN := recursionMakeNode(4)
+	printNode(tmpN)
+	fmt.Printf("result:%t\n",isPalindrome(tmpN))
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+//遍历输出链表
+func printNode(NodeP *ListNode){
+	for NodeP != nil{
+		fmt.Printf("Val:%d\n",NodeP.Val)
+		NodeP = NodeP.Next
+	}
+}
+//反转链表
+func reverseList(head *ListNode) *ListNode {
+	var tmpNode *ListNode
+	if head == nil || head.Next == nil{
+		return head
+	}else{
+		tmpNode = reverseList(head.Next)
+		head.Next.Next = head
+		head.Next = nil
+		return tmpNode
+	}
+}
+
+//时间复杂度O(n)有点难啊，
+//先试一下快慢指针的方法
+//需要调用反转链表reverseList()
+func isPalindrome(head *ListNode) bool {
+    if head == nil || head.Next == nil{
+        return true
+    }
+	var fast_point *ListNode
+	var slow_point *ListNode
+	fast_point = head
+	slow_point = head
+
+	for fast_point.Next != nil && fast_point.Next.Next != nil{
+		slow_point = slow_point.Next
+		fast_point = fast_point.Next.Next
+	}
+	// slow_point = slow_point.Next
+	slow_point = reverseList(slow_point.Next)
+	for slow_point != nil {
+		if slow_point.Val == head.Val{
+			slow_point = slow_point.Next
+			head = head.Next
+		}else{
+			return false
+		}
+	}
+	return true
+}
+
+
+//n指的是i递增到的最大值而不是链表的长度
+//这里没有用递归
+//创造一个回文链表方便测试
+func recursionMakeNode(n int) *ListNode{
+
+	var point *ListNode
+	if n < 1{
+		return nil
+	}
+
+	head := ListNode{Val:1}
+	if n==1{
+		return &head
+	}
+	point = &head
+	for i:=2;i<=n;i++{
+		newNode := ListNode{Val:i}
+		point.Next = &newNode
+		point = point.Next
+	}
+	for j:=n;j>0;j--{
+		newNode := ListNode{Val:j}
+		point.Next = &newNode
+		point = point.Next
+	}
+	return &head
+}
+
+
+
+
+
