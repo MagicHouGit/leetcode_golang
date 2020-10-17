03dad05912cd41d3cdee7d362c805cec484b26b8
OMPower <magichou13@gmail.com>
2020-10-17 20:17:24 +0800

    first commit

---
+++ 155gitMin.go
@@ -0,0 +1,135 @@
+package main
+
+import "fmt"
+
+func main() {
+	// MinStack minStack := new MinStack();
+	// minStack.push(-2);
+	// minStack.push(0);
+	// minStack.push(-3);
+	// minStack.getMin();
+	// minStack.pop();
+	// minStack.top();
+	// minStack.getMin();
+	obj := Constructor()
+	obj.Push(2147483646)
+	obj.Push(2147483646)
+	obj.Push(2147483647)
+	obj.Top()
+	obj.Pop()
+	param_4 := obj.GetMin()
+	fmt.Println(param_4)
+	obj.Pop()
+	param_4 = obj.GetMin()
+	fmt.Println(param_4)
+	obj.Pop()
+	obj.Push(2147483647)
+	obj.Top()
+	param_4 = obj.GetMin()
+	fmt.Println(param_4)
+
+	// param_3 := obj.Top()
+	// param_4 = obj.GetMin()
+
+	// fmt.Println(param_3)
+	// fmt.Println(param_4)
+}
+
+type MinStack struct {
+	data    []int
+	minData []int
+}
+
+/** initialize your data structure here. */
+func Constructor() MinStack {
+	return MinStack{
+		data:    make([]int, 0),
+		minData: make([]int, 0),
+	}
+}
+
+func (this *MinStack) Push(x int) {
+	this.data = append(this.data, x)
+	if len(this.minData) == 0 {
+		this.minData = append(this.minData, x)
+	} else if x < this.minData[len(this.minData)-1] {
+		this.minData = append(this.minData, x)
+	} else {
+		this.minData = append(this.minData, this.minData[len(this.minData)-1])
+	}
+}
+
+func (this *MinStack) Pop() {
+	if len(this.data) == 0 {
+		return
+	}
+	length := len(this.data)
+	this.data = this.data[:length-1]
+	this.minData = this.minData[:length-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.data[len(this.data)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minData[len(this.minData)-1]
+}
+
+// ============================================
+// // 执行用时：60 ms, 在所有 Go 提交中击败了8.18%的用户
+// // 内存消耗：10 MB, 在所有 Go 提交中击败了6.07%的用户
+// // MinStack int
+// type MinStack struct {
+// 	n []int
+// }
+
+// /** initialize your data structure here. */
+// func Constructor() MinStack {
+// 	return MinStack{}
+// }
+
+// func (this *MinStack) Push(x int) {
+// 	this.n = append(this.n, x)
+// }
+
+// func (this *MinStack) Pop() {
+// 	if len(this.n) == 1 {
+// 		this.n = nil
+// 	}
+// 	if len(this.n) > 1 {
+
+// 		this.n = this.n[:len(this.n)-1]
+// 	}
+
+// }
+
+// func (this *MinStack) Top() int {
+
+// 	res := this.n[len(this.n)-1]
+// 	// this.Pop()
+// 	return res
+// }
+
+// func (this *MinStack) GetMin() int {
+
+// 	if this.n == nil {
+// 		return 0
+// 	}
+// 	res := this.n[0]
+// 	for _, v := range this.n {
+// 		if res > v {
+// 			res = v
+// 		}
+// 	}
+// 	return res
+// }
+//====================================
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */
