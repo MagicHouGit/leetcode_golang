a418ecc166bbba4e8da456b1ccfc610d8dfa0582
OMPower <magichou13@gmail.com>
2020-10-25 23:41:11 +0800

    add commit

--- 763partitionLabels.go
+++ 763partitionLabels.go
@@ -1,3 +1,19 @@
+// 763. 划分字母区间
+// 字符串 S 由小写字母组成。我们要把这个字符串划分为尽可能多的片段，同一字母最多出现在一个片段中。返回一个表示每个字符串片段的长度的列表。
+
+// 示例：
+
+// 输入：S = "ababcbacadefegdehijhklij"
+// 输出：[9,7,8]
+// 解释：
+// 划分结果为 "ababcbaca", "defegde", "hijhklij"。
+// 每个字母最多出现在一个片段中。
+// 像 "ababcbacadefegde", "hijhklij" 的划分是错误的，因为划分的片段数较少。
+
+// 提示：
+
+// S的长度在[1, 500]之间。
+// S只包含小写字母 'a' 到 'z' 。
 package main
 
 import "fmt"
@@ -27,11 +43,13 @@ func partitionLabels(S string) []int {
 	if len(S) == 0 {
 		return nil
 	}
-
+	//记录不重复的字母
 	var repeat []byte
+	//和repeat的位置对应，这个字母的起始位置和结束位置，
 	var to [][]int
 	var res []int
 
+	//
 	repeat = append(repeat, S[0])
 	to = append(to, []int{0, 0})
 	for i, v := range S {
@@ -68,6 +86,8 @@ func partitionLabels(S string) []int {
 		// 	end = to[i+1][1]
 		// 	continue
 		// }
+		//这个字符的起始是否大于之前字符的结束，
+		//大于就代表一个段落的结束，从当前字符开启一个新的段落
 		if to[i][0] > end {
 			res = append(res, end-start+1)
 			remain -= end - start + 1
